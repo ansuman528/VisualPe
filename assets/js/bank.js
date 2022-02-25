cd4b9973b1276c76c64842f575562c54885e4d3c
Sayansree Paria <sayansreeparia@gmail.com>
2022-02-25 20:53:48 +0530

    dynamic bank

--- assets/js/bank.js
+++ assets/js/bank.js
@@ -1,4 +1,4 @@
-
+var deposit =""
 
 const getData =(type,callback)=>{
     fetch(`https://bridge-test-api.herokuapp.com/get-data/${type}`,
@@ -18,14 +18,52 @@ const getData =(type,callback)=>{
         console.log("connection error")
     })
 }
+const Commas=(x)=> {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 const insert=(id,data)=>{
     if ($(`#${id}`).length) {
       $(`#${id}`).get(0).innerHTML=data
     }
   }
 $( ()=> {
-
-
+    getData('DEPOSIT',(data)=>{
+        deposit=data;
+        var bankList=$("#bank-list").get(0)
+        var bankTile=$("#bank-tile").get(0)
+        const thm=["bg-gradient-danger","bg-gradient-info","bg-gradient-success"]
+        var cnt=0
+        bankList.innerHTML=""
+        bankTile.innerHTML=""
+        for (i of data.Payload[0].data){
+             i=i.decryptedFI.account
+             bankList.innerHTML+=`<input  type="checkbox"><strong style="color: cadetblue;"> ${i.profile.holders.holder[0].name} </strong> <br> a/c: ${i.maskedAccNumber}</input><hr>`
+            bankTile.innerHTML+=`<div class="col-md-4 stretch-card grid-margin">
+            <div class="card ${thm[cnt]} card-img-holder text-white">
+              <div class="card-body">
+                <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image">
+                <h4 class="font-weight-normal mb-3">Current Balance<i class="mdi mdi-diamond mdi-24px float-right"></i>
+                </h4>
+                <h2 class="mb-5">₹ ${Commas(i.summary.currentBalance)}</h2>
+                <h5 class="card-text">Branch: ${i.summary.branch}</h5>
+                <h6 class="card-text">MICR: ${i.summary.ifscCode}</h6>
+                <h6 class="card-text">IFSC: ${i.summary.ifscCode}</h6>
+              </div>
+            </div>
+          </div>`
+          cnt=(cnt+1)%3
+            }
+        bankList.innerHTML+=`<span type="button" onclick="signout()">Sign Out</span>`
+        // i.profile.holders.holder[0].name
+        // 
+        // i.summary.branch
+        // i.summary.ifscCode
+        // i.summary.micrCode
+        // i.summary.currentBalance
+        // Bar("fd-bar",[data.summary.principalAmount,data.summary.currentValue],["invested Value","Current Value"])
+        // insert("fd-i",data.summary.interestRate+"%")
+        // insert("fd-md",data.summary.maturityDate)
+    })
     getData('TERM_DEPOSIT',(data)=>{
         Bar("fd-bar",[data.summary.principalAmount,data.summary.currentValue],["invested Value","Current Value"])
         insert("fd-i",data.summary.interestRate+"%")
