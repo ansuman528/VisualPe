1a7a484a9166321f3ab3dceb08fb7da7fb9ca9bc
Sayansree Paria <sayansreeparia@gmail.com>
2022-02-27 11:28:01 +0530

    fix

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -207,7 +207,7 @@ const logout=()=>{
   localStorage.setItem("jwt",null)
   window.location.href="https://ansuman528.github.io/VisualPe/login.html"
 }
-if(window.location.href.substring(0,37) == "https://ansuman528.github.io/VisualPe"){
+// if(window.location.href.substring(0,37) == "https://ansuman528.github.io/VisualPe"){
 var jwt=localStorage.getItem("jwt")
 if(jwt==null)window.location.href="https://ansuman528.github.io/VisualPe/login.html"
 insert("phn-no",JSON.parse(window.atob(jwt.split('.')[1])).phone)
@@ -233,5 +233,5 @@ fetch(`https://bridge-test-api.herokuapp.com/checklogin`,
       // $(sub).get(0).innerHTML=`<h3>Oops something went wrong ...</h3><h4>Try again </h4><h5>Reloading page ...</h5>`
       // setTimeout(()=>window.location.reload(),5000)
   })
-}
+// }
 
